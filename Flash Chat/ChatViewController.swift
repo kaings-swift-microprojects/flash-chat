65ba3fbf59232c7d05c88d7f11198b917397c4b4
w1lltj <cycorn8@gmail.com>
2019-08-14 16:01:40 +0800

    create user signout

--- Flash Chat/ChatViewController.swift
+++ Flash Chat/ChatViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 
 class ChatViewController: UIViewController {
@@ -107,7 +108,15 @@ class ChatViewController: UIViewController {
     @IBAction func logOutPressed(_ sender: AnyObject) {
         
         //TODO: Log out the user and send them back to WelcomeViewController
-        
+        do {
+            try Auth.auth().signOut()
+            navigationController?.popToRootViewController(animated: true)
+            
+        } catch let err as NSError {
+            print("Error! there was problem signing out.")
+            print(err)
+        }
+
         
     }
     
