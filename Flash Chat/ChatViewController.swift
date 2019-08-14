5eb2bed9b16a9e79053200016c40037a8a5e0ae1
w1lltj <cycorn8@gmail.com>
2019-08-14 17:39:38 +0800

    set CustomMessageCell(UITableViewCell) in the UITableView to be displayed

--- Flash Chat/ChatViewController.swift
+++ Flash Chat/ChatViewController.swift
@@ -10,10 +10,10 @@ import UIKit
 import Firebase
 
 
-class ChatViewController: UIViewController {
+class ChatViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     // Declare instance variables here
-
+    var messageArray = ["First Message", "Second Message bla bla bla bla bla", "Third Message bla bla"]
     
     // We've pre-linked the IBOutlets
     @IBOutlet var heightConstraint: NSLayoutConstraint!
@@ -27,8 +27,8 @@ class ChatViewController: UIViewController {
         super.viewDidLoad()
         
         //TODO: Set yourself as the delegate and datasource here:
-        
-        
+        messageTableView.delegate = self    // all actions towards tableview will be notified (delegated) to ChatViewController
+        messageTableView.dataSource = self  // all datasource to tableview should come from ChatViewController
         
         //TODO: Set yourself as the delegate of the text field here:
 
@@ -38,8 +38,8 @@ class ChatViewController: UIViewController {
         
         
 
-        //TODO: Register your MessageCell.xib file here:
-
+        //TODO: Register your MessageCell.xib file here: Register the CustomMessageCell in order to be able to use
+        messageTableView.register(UINib(nibName: "MessageCell", bundle: nil), forCellReuseIdentifier: "customMessageCell")
         
     }
 
@@ -50,11 +50,24 @@ class ChatViewController: UIViewController {
     
     
     //TODO: Declare cellForRowAtIndexPath here:
-    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        print(indexPath)
+        print(indexPath.section)    // section is like column
+        print(indexPath.row)
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "customMessageCell", for: indexPath) as! CustomMessageCell   // modify each cell (UITableViewCell) to be loaded in the UITableView to use our CustomMessageCell (identifier "customMessageCell", MessageCell.xib as registered in the viewDidLoad() above)
+        
+        cell.messageBody.text = messageArray[indexPath.row]
+        
+        return cell
+    }
     
     
     //TODO: Declare numberOfRowsInSection here:
-    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messageArray.count   // return number of cells to be displayed (the content of the cell should not be empty or else error). Therefore, should be same as number of content to be put in the cell
+    }
     
     
     //TODO: Declare tableViewTapped here:
